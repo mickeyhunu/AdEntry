aa060972f25e134d07ba0b2eb5e2dfb4d1905285
mickeyhunu <mickeyhunu@gmail.com>
2025-09-26 01:56:47 +0900

    Update app.js

--- app.js
+++ app.js
@@ -10,9 +10,6 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
